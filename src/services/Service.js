218260c88f81f7daf28bfbfa2a4da83ba5ff2834
Intrepidpay <ajetship@gmail.com>
2026-05-16 18:01:58 +0100

    Update fake shipping data and status to 'In Transit'

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,63 +1,108 @@
 
 const fakeShippingData = {
  SH82352287: {
-    sender: 'Olivia Tooley',
-    recipient: 'Ryo Kuratomi',
-    contact: '+81 (80) 3788-7802',
-    address: 'Apt 204 Gran Paseo, 2-17-46 Okubo, Shinjuku, Tokyo 169-0072, Japan',
+    sender: '',
+    recipient: 'Debra cox',
+    contact: '+1(830)992-0290',
+    address: '312 ECKHARDT LANE FREDERICKSBURG,TX,78624,USA',
     method: 'Express (2-3 days)',
-    status: 'Out for Delivery',
+    status: 'In Transit',
     trackingId: 'SH82352287',
     orderSummary: {
       shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 1177.00, paid: true },
-      tax: { amount: 23.22, paid: true },
-      total: 0.00
+      clearance: { amount: 285.00, paid: false },
+      tax: { amount: 23.22, paid: false },
+      total: 318.22
     }
    },
   SH82437295: {
-    sender: 'Olivia Tooley',
-    recipient: 'Satoru Ueno',
-    contact: '+81 (90) 7716-1223',
-    address: '4-13-3 Tatekawa Sumida-ku, Tokyo 130-0023 Japan',
+    sender: '',
+    recipient: 'Austin Sharp',
+    contact: '+1(432)853-6978',
+    address: '1405 S COUNTY ROAD 1110 MIDLAND,TX,79706,USA',
     method: 'Express (2-3 days)',
-    status: 'Pending Payment',
+    status: 'In Transit',
     trackingId: 'SH82437295',
     orderSummary: {
       shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 265.00, paid: false },
+      clearance: { amount: 285.00, paid: false },
       tax: { amount: 23.22, paid: false },
-      total: 288.22
+      total: 318.22
    }
   },
   SH86737495: {
-    sender: 'Olivia Tooley',
-    recipient: 'Joey',
-    contact: '+1 (478) 538-8354',
-    address: '103 Witherspoon Court, Warner Robins, GA 31088, USA',
+    sender: '',
+    recipient: 'Jeffery Jacops',
+    contact: '+1(703)980-9043',
+    address: '4216 MORNING DRIVE AMARILLO,TX,79108,USA',
     method: 'Express (2-3 days)',
-    status: 'Pending Payment',
+    status: 'In Transit',
     trackingId: 'SH86737495',
     orderSummary: {
       shippingFee: { amount: 350.99, paid: true },
       clearance: { amount: 285.00, paid: false },
-      tax: { amount: 43.22, paid: false },
-      total: 328.22
+      tax: { amount: 23.22, paid: false },
+      total: 318.22
      }
     },
     SH82152286: {
-    sender: 'Olivia Tooley',
-    recipient: 'Hiromi Kubo',
-    contact: '----',
-    address: 'Prejiel 101, Sakaecho 4-166, Ushiku, Ibaraki 300-1233, Japan',
+    sender: '',
+    recipient: 'Linda Finger',
+    contact: '+1(210)912-0940',
+    address: 'PO BOX 278 D HANIS,TX,78850,USA',
     method: 'Express (2-3 days)',
-    status: 'Pending Payment',
+    status: 'In Transit',
     trackingId: 'SH82152286',
     orderSummary: {
       shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 265.00, paid: false },
+      clearance: { amount: 285.00, paid: false },
       tax: { amount: 23.22, paid: false },
-      total: 288.22
+      total: 318.22
+    }
+  },
+    SH82152576: {
+    sender: '',
+    recipient: 'Paul Friedrichs',
+    contact: '+1(210)617-8061',
+    address: '14403 STAR CROSS TRL HELOTES,TX,78023,USA',
+    method: 'Express (2-3 days)',
+    status: 'In Transit',
+    trackingId: 'SH82152576',
+    orderSummary: {
+      shippingFee: { amount: 350.99, paid: true },
+      clearance: { amount: 285.00, paid: false },
+      tax: { amount: 23.22, paid: false },
+      total: 318.22
+    }
+  },
+    SH82153346: {
+    sender: '',
+    recipient: 'Tamara Huckaby',
+    contact: '+1(432)360-9217',
+    address: '1801 W 6TH ST FORT STOCKTON,TX,79735,USA',
+    method: 'Express (2-3 days)',
+    status: 'In Transit',
+    trackingId: 'SH82153346',
+    orderSummary: {
+      shippingFee: { amount: 350.99, paid: true },
+      clearance: { amount: 285.00, paid: false },
+      tax: { amount: 23.22, paid: false },
+      total: 318.22
+    }
+  }, 
+    SH81764286: {
+    sender: '',
+    recipient: 'Lovelyn Wharton',
+    contact: '+1(830)719-0716',
+    address: '404 AGUA VERDE DEL RIO,TX,78840,USA',
+    method: 'Express (2-3 days)',
+    status: 'In Transit',
+    trackingId: 'SH81764286',
+    orderSummary: {
+      shippingFee: { amount: 350.99, paid: true },
+      clearance: { amount: 285.00, paid: false },
+      tax: { amount: 23.22, paid: false },
+      total: 318.22
     }
   }
 };
