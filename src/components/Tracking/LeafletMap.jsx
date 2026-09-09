0ba4c516f51f06a73eec07bca51518c7e7ba5db4
Intrepidpay <ajetship@gmail.com>
2026-09-09 19:04:33 +0100

    Update map tile layer to use Stadia Maps

--- src/components/Tracking/LeafletMap.jsx
+++ src/components/Tracking/LeafletMap.jsx
@@ -68,9 +68,9 @@ const LeafletMap = ({ locations = [], progress = 0 }) => {
         maxZoom={11}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        />
+  url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=ec2d145d-9b60-4824-a3e6-bf5b1bf5185d"
+  attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+/>
         
         {locations.map((location, index) => (
           <Marker 
