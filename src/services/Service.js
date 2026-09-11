859978c79ac8ae95ead30443e0d7c00b595cd54b
Intrepidpay <ajetship@gmail.com>
2026-09-11 01:42:35 +0100

    Refactor fakeShippingData and update getShippingDetails

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,21 +1,5 @@
 
 const fakeShippingData = {
- SH82352287: {
-    sender: 'Surprise Giftplug',
-    recipient: 'Debra cox',
-    contact: '+1(830)992-0290',
-    address: '312 ECKHARDT LANE FREDERICKSBURG,TX,78624,USA',
-    method: 'Express (2-3 days)',
-    stripe: '',
-    status: 'Pending Payment',
-    trackingId: 'SH82352287',
-    orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
-    }
-   },
   SH82437295: {
     sender: 'Surprise Giftplug',
     recipient: 'Austin Sharp',
@@ -26,85 +10,26 @@ const fakeShippingData = {
     status: 'Pending Payment',
     trackingId: 'SH82437295',
     orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
+    shippingFee: { amount: 350.99, paid: true },
+    clearance: { amount: 285.00, paid: false },
+    tax: { amount: 23.22, paid: false },
+    total: 318.22
    }
   },
+ 
   SH86737495: {
-    sender: 'Surprise Giftplug',
-    recipient: 'Jeffery Jacops',
-    contact: '+1(703)980-9043',
-    address: '4216 MORNING DRIVE AMARILLO,TX,79108,USA',
+    sender: 'Olivia Tooley',
+    recipient: 'Joe Happer',
+    contact: '+1 (806) 3558-7843',
+    address: 'Apt 204, 2-17-46 Dallas Park, Texas 169-0072, United States',
     method: 'Express (2-3 days)',
     status: 'Pending Payment',
     trackingId: 'SH86737495',
     orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
-     }
-    },
-    SH82152286: {
-    sender: 'Surprise Giftplug',
-    recipient: 'Linda Finger',
-    contact: '+1(210)912-0940',
-    address: 'PO BOX 278 D HANIS,TX,78850,USA',
-    method: 'Express (2-3 days)',
-    status: 'Pending Payment',
-    trackingId: 'SH82152286',
-    orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
-    }
-  },
-    SH82152576: {
-    sender: 'Surprise Giftplug',
-    recipient: 'Paul Friedrichs',
-    contact: '+1(210)617-8061',
-    address: '14403 STAR CROSS TRL HELOTES,TX,78023,USA',
-    method: 'Express (2-3 days)',
-    status: 'Pending Payment',
-    trackingId: 'SH82152576',
-    orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
-    }
-  },
-    SH82153346: {
-    sender: 'Surprise Giftplug',
-    recipient: 'Tamara Huckaby',
-    contact: '+1(432)360-9217',
-    address: '1801 W 6TH ST FORT STOCKTON,TX,79735,USA',
-    method: 'Express (2-3 days)',
-    status: 'Pending Payment',
-    trackingId: 'SH82153346',
-    orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
-    }
-  }, 
-    SH81764286: {
-    sender: 'Surprise Giftplug',
-    recipient: 'Lovelyn Wharton',
-    contact: '+1(830)719-0716',
-    address: '404 AGUA VERDE DEL RIO,TX,78840,USA',
-    method: 'Express (2-3 days)',
-    status: 'Pending Payment',
-    trackingId: 'SH81764286',
-    orderSummary: {
-      shippingFee: { amount: 350.99, paid: true },
-      clearance: { amount: 285.00, paid: false },
-      tax: { amount: 23.22, paid: false },
-      total: 318.22
+    shippingFee: { amount: 350.99, paid: true },
+    clearance: { amount: 285.00, paid: false },
+    tax: { amount: 23.22, paid: false },
+    total: 318.22
     }
   }
 };
